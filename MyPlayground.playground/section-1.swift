30e8856629e148aa4f3cb81cbe3ebcb72d537c82
TJ Marbois <tmarbois@pc14.home>
2014-09-30 18:16:17 +0200

    First Swift commits
    woo hoo!

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+println(str)
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+let implicitInteger = 6
+let implicitDouble = 6.0
+let explicitDouble: Double = 70
+
+let aConstant: Float = 4
+
+let thelabel = "The Width is "
+let width = 94
+let widthLabel = thelabel + String(width)
+
+let apples: Float = 3
+let oranges: Float = 5
+let appleSummary = "I have \(apples) apples!"
+let fruitSummary = "I have \(apples + oranges) pieces of fruit!"
+
+
+
+
+
