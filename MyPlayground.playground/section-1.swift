f68d32404e08068e30c317cbc24733061178935e
TJ Marbois <tmarbois@pc14.home>
2014-10-01 20:57:59 +0200

    for in stuffs

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -96,6 +96,27 @@ default:
     
 }
 
+// for in stuff
+
+let interestingNumbers = [
+    "Prime": [2,3,5,7,11,13],
+    "Fibonacci": [1,1,3,5,8],
+    "Square": [1,4,9,16,25],
+]
+
+var largest = 0
+
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+
+largest
+
+
 
 
 
