4c54166859c9fbe473e0ba2325bb1258d749042e
TJ Marbois <tmarbois@pc14.home>
2014-10-01 22:14:41 +0200

    function using a damn function to do NOTHING

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -199,6 +199,22 @@ averageOf(12, 14, 35, 100)
 
 
 
+// nested fucntions yo
+
+func returnFifteen() -> Int
+{
+    var y = 10
+    func add()
+    {
+        y += 5
+    }
+    add()
+    return y
+}
+
+returnFifteen()
+
+
 
 
 
