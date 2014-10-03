2d4f45804c75830536ee244efa9a859c72bf8199
TJ Marbois <tmarbois@pc14.home>
2014-10-03 21:47:49 +0200

    func that takes a func!

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -229,6 +229,30 @@ var increment = makeIncrementer()
 increment(7)
 
 
+// function that takes a function
+
+func hasAnyMatches(list: Int[], condition: Int -> Bool) -> Bool
+{
+    for item in list
+    {
+        if condition(item)
+        {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool
+{
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 12]
+
+hasAnyMatches(numbers, lessThanTen)
+
+
 
 
 
